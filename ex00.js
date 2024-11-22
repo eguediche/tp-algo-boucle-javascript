6c9d5e9327d563a071f7fed0fdf014027ac5296c
eguediche <eguediche@edenschool.fr>
2024-11-22 14:05:28 +0100

    ex00.js

---
+++ ex00.js
@@ -0,0 +1,9 @@
+// ex00.js
+
+function sumArray(arr) {
+	// Écrire le code ici
+}
+
+// Exemples de test :
+console.log(sumArray([1, 2, 3, 4])); // Résultat attendu : 10
+console.log(sumArray([10, -2, 5])); // Résultat attendu : 13
